301a89223883fc1afe1e60d2db5e70000692cd9f
Riya Chaurasia <155444751+Chaurasiariya227@users.noreply.github.com>
2026-07-23 21:49:45 +0530

    Connect frontend to deployed backend

--- client/src/api/adminApi.js
+++ client/src/api/adminApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:5000/api/appointments";
+const API = `${import.meta.env.VITE_API_URL}/api/appointments`;
 
 const getToken = () => localStorage.getItem("token");
 
--- client/src/api/appointmentApi.js
+++ client/src/api/appointmentApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:5000/api/appointments";
+const API = `${import.meta.env.VITE_API_URL}/api/appointments`;
 
 export const bookAppointment = async (appointmentData) => {
 
